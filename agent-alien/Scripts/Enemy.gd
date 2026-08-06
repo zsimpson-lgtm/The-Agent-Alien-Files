280e45341e3597bc3188f05cf63097699b45266c
zsimpson-lgtm <zsimpson@students.mags.school.nz>
2026-08-07 10:39:45 +1200

    Day 16
    Worked on area monitoring made the attack sensoring more fluid just needa make the animation for walking dif as i want guns out at all times and add a collision shape as a fix for the x inversion not working with the root nodes coll shape

--- agent-alien/Scripts/Enemy.gd
+++ agent-alien/Scripts/Enemy.gd
@@ -5,12 +5,13 @@ var gravity = 980
 @export var enemy_health: int = 1
 var spawn
 @onready var spawner: PackedScene = preload("res://Scenes/Enemy.tscn")
-var attack_frames = [6, 7, 8]
+var attack_frames 
 var body
 @onready var player = get_parent().get_node("Player")
 @onready var enemy = get_parent().get_node("Enemy")
 @onready var player_attack = player.get_node("AnimatedSprite2D")
 var in_range: bool = false
+@onready var attack_area = player.get_node("Area2D2")
 
 func _ready() -> void:
 	pass
@@ -23,10 +24,15 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
+func _process(_delta):
+	if player_attack.animation == "Attack" and player_attack.frame == 1:
+		attack_area.monitoring = player_attack.frame in [1, 2, 3]
+	else:
+		attack_area.monitoring = false
 
 
 func _on_area_2d_area_entered(body: Area2D) -> void:
-	if body.is_in_group("Player") and player_attack.animation == "Attack" and player_attack.frame in attack_frames:
+	if body.is_in_group("Player") and player_attack.animation == "Attack":
 		take_damage()
 
 func take_damage() -> void:
